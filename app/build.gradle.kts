09fff85c6763930a704e10e50bb408483d154917
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2026-02-17 23:48:50 +0700

    Configure BASE_URL in BuildConfig and update ApiConfig to use it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        
+        buildConfigField("String", "BASE_URL", "\"https://event-api.dicoding.dev/\"")
     }
 
     buildTypes {
@@ -34,6 +36,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
